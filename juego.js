645dbcdcdd31554ac952be7c41cd34f9ab31a547
David Fuertes <Davidfuertesrojas@gmail.com>
2024-01-09 20:29:11 +0100

    versión 2.0 de aleatoriedad incluyendo rutas de imagen

--- juego.js
+++ juego.js
@@ -1,12 +1,49 @@
-//inicio de variables
 let untapped = 0;
+const totalButtons = 16;
+
+// Randomizador
+let cards = {
+    1: 'assets/emoji1.jpg',
+    2: 'assets/emoji2.jpg',
+    3: 'assets/emoji3.jpg',
+    4: 'assets/emoji4.jpg',
+    5: 'assets/emoji5.jpg',
+    6: 'assets/emoji6.jpg',
+    7: 'assets/emoji7.jpg',
+    8: 'assets/emoji8.jpg',
+    9: 'assets/emoji1.jpg',
+    10: 'assets/emoji2.jpg',
+    11: 'assets/emoji3.jpg',
+    12: 'assets/emoji4.jpg',
+    13: 'assets/emoji5.jpg',
+    14: 'assets/emoji6.jpg',
+    15: 'assets/emoji7.jpg',
+    16: 'assets/emoji8.jpg',
+};
+
+let cardsArray = Object.entries(cards);
+cardsArray.sort(() => Math.random() - 0.5);
+
+// Ponemos cada uno de los resultados en cada uno de los botones
+let buttonResultsMap = {};
+for (let i = 0; i < totalButtons; i++) {
+    const buttonId = i + 1;
+    const cardEntry = cardsArray[i];
+    buttonResultsMap[buttonId] = cardEntry[1];
+}
 
 
-//Randomizador
-let cards =[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
-cards = cards.sort(() => { 
-    return Math.random() -0.5
-})
-console.log(cards);
+// untap
+function untap(buttonId) {
+    untapped++;
 
-//Luego, cuando tengamos el "flip" de las cartas, sustituiré los nºs por las imágenes, y las asociaré al botón
+
+    const result = buttonResultsMap[buttonId];
+    console.log(`Botón ${buttonId} seleccionado`);
+    console.log(`Imagen asociada: ${result}`);
+}
+
+
+untap(1);
+untap(2);
+
